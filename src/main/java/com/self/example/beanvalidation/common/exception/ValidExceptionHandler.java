5e9b21725d4822a69e116da019f68c56f351a4fe
zzw <1032983946@qq.com>
2021-02-19 01:38:25 +0800

    feat validExceptionHnader

--- src/main/java/com/self/example/beanvalidation/common/exception/ValidExceptionHandler.java
+++ src/main/java/com/self/example/beanvalidation/common/exception/ValidExceptionHandler.java
@@ -4,6 +4,7 @@ import com.self.example.beanvalidation.common.enums.CommonCodeEnum;
 import com.self.example.beanvalidation.common.response.BaseResponse;
 import com.self.example.beanvalidation.common.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,18 +13,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
-public class CommonExceptionHandler {
+public class ValidExceptionHandler {
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     @ResponseBody
     public Response validExceptionHandler(MethodArgumentNotValidException e) {
-        List<String> result = new ArrayList<>();
-        e.getAllErrors().forEach(error -> result.add(((FieldError) error).getField() + error.getDefaultMessage()));
-        log.error("参数校验异常！", e);
+        Map<String, String> result = new HashMap<>(1>>3);
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            result.put(error.getField(), error.getDefaultMessage());
+        });
         return new BaseResponse<>(CommonCodeEnum.FAILED, result);
     }
 
